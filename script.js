f39bd4e858f85b4bfb6c5492016674dbb92bed09
mgemzon <gemzon.markchristopher@gmail.com>
2025-04-23 19:12:15 +0800

    Add UI

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
-// Generate choice from computer
-
-// Get user choice
-
-// Compare user choice and computer choice
-//   IF uc and cc are equal it is a tie.
-//   ELIF uc is rock and cc is scissors OR
-//        uc is paper and cc is rock OR
-//        uc is scissors and cc is paper
-//          uc win
-//   ELSE cc win
-//      cc win
-
 const rock = "rock";
 const paper = "paper"
 const scissors = "scissors";
 
 let userScore = 0;
 let computerScore = 0;
+
+let div = document.querySelector("#display");
 
 function generateComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
@@ -30,47 +19,52 @@ function generateComputerChoice() {
     }
 }
 
-function getUserChoice() {
-    let userChoice = prompt("Rock, paper, scissors?");
-    userChoice = userChoice.toLowerCase().trim();
-    if (!(userChoice === rock || userChoice === paper || userChoice === scissors)) {
-        // throw new Error("Invalid choice!");
-    }
-    return userChoice;
-}
-
 function playRound(userChoice, computerChoice) {
-    console.log(`Your choice: ${userChoice}\nOpponent choice: ${computerChoice}`);
+    let message = `Your choice: ${userChoice}\nOpponent choice: ${computerChoice}`;
     if (userChoice === computerChoice) {
-        console.log("Tie!");
+        message += "<br>Tie!";
         // no action
     } else if ((userChoice === rock && computerChoice === scissors) ||
         (userChoice === paper && computerChoice === rock) ||
         (userChoice === scissors && computerChoice === paper)) {
-        console.log("Round won!");
+        message += "<br>Round won!";
         userScore++;
     } else {
-        console.log("Round lost!");
-        computerChoice++;
+        message += "<br>Round lost!";
+        computerScore++;
     }
+
+    message += `<br>Score. You: ${userScore} Computer: ${computerScore}`;
+    div.innerHTML = message;
 }
 
-function playGame() {
-    playRound(getUserChoice(), generateComputerChoice());
-    playRound(getUserChoice(), generateComputerChoice());
-    playRound(getUserChoice(), generateComputerChoice());
-    playRound(getUserChoice(), generateComputerChoice());
-    playRound(getUserChoice(), generateComputerChoice());
-
-    console.log(`Final score. You: ${userScore} Computer: ${computerScore}`);
+function displayFinalResult() {
+    let message = `Final score. You: ${userScore} Computer: ${computerScore}`;
 
     if (userScore > computerScore) {
-        console.log("YOU WON!");
+        message += "<br>YOU WON!";
     } else if (userScore < computerScore) {
-        console.log("YOU LOST!");
+        message += "<br>YOU LOST!";
     } else {
-        console.log("IT'S A TIE.");
+        message += "<br>IT'S A TIE!";
     }
+
+    div.innerHTML = message;
 }
 
-playGame();
+window.addEventListener("click", (event) => {
+    if (userScore === 5 || computerScore === 5) {
+        return;
+    }
+
+    if (event.target.tagName === "BUTTON") {
+        const userChoice = event.target.id;
+        const computerChoice = generateComputerChoice();
+        playRound(userChoice, computerChoice);
+    }
+
+    if (userScore === 5 || computerScore === 5) {
+        displayFinalResult();
+    }
+});
+
